0bcd202d5e94925c0f6093c932da79a2315756d5
aganve <verda.e.agan@vanderbilt.edu>
2019-09-26 12:20:10 -0500

    added comments to pso_necroptosis file

--- pso_necroptosis.py
+++ pso_necroptosis.py
@@ -9,7 +9,7 @@ from simplepso.pso import PSO
 import collections​
 model.enable_synth_deg()
 
-# Defining our observable
+# Declaring our observable: phosphorylated MLKL
 obs_names = ['MLKLp_obs']
 mlklp_obs = 'MLKLp_obs'
 
@@ -27,10 +27,10 @@ t = np.linspace(0, 960, num=11)
 solver1 = ScipyOdeSimulator(model, tspan=t)
 
 # Creating a grid of values, all of the same type, and is indexed by a list of non-negative integers (i.e., tuple)
-timex = np.array([0,2,4,6,8,10,12,14,16,18, 20])
-normvaluesy = np.array([0., 0.001, 0.02, 0.03, 0.04, 0.06, .09, .21, .40, .65, .81])
+x10 = np.array([0,2,4,6,8,10,12,14,16,18, 20])
+y10 = np.array([0., 0.001, 0.02, 0.03, 0.04, 0.06, .09, .21, .40, .65, .81])
 
-ydata_norm = normvaluesy
+ydata_norm = y10
 
 rate_params = model.parameters_rules()
 param_values = np.array([p.value for p in model.parameters])
@@ -49,7 +49,7 @@ log10_original_values = np.log10(original_values[rate_mask])
 # Returns a tuple: a sequence that cannot be changed unlike lists; uses parantheses
 def obj_function(params):
     params_tmp = np.copy(params)
-    param_values[rate_mask] = 10 ** params_tmp  #don't need to change
+    param_values[rate_mask] = 10 ** params_tmp  # don't need to change
     result = solver1.run(param_values=param_values)
     ysim_array1 = result.observables['MLKLp_obs'][:]
     ysim_norm1 = normalize(ysim_array1)
@@ -61,6 +61,7 @@ def obj_function(params):
   counter = 0
     # Here, we initial the class
     # We must provide the cost function and a starting value
+    # Provide the bounds, speed, particle, and iterations count
     for i in range(1000):
         optimizer = PSO(cost_function=obj_function,start = log10_original_values, verbose=True)
         # We also must set bounds. This can be a single scalar or an array of len(start_position)
