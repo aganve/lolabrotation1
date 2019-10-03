e22cbb17e3a1035f5f6f46de6637889600546fda
aganve <verda.e.agan@vanderbilt.edu>
2019-10-03 17:21:25 -0500

    modified necroptosis.py file

--- necroptosis.py
+++ necroptosis.py
@@ -13,7 +13,7 @@ Model()
 # declare monomers
 Monomer('TNF', ['bCI'])
 Monomer('CI', ['bTNF','state'], {'state':['i','a']})
-Monomer('CII', ['bCI','state'], {'state': ['a']})
+Monomer('CII', ['bMLKL','state'], {'state': ['a']})
 Monomer('MLKL', ['bCII', 'state'], {'state': ['u','p']})
 
 # input the parameter values
@@ -28,9 +28,9 @@ Parameter('kf5', 1.0e-6)
 # now input the rules
 Rule('TNF_CI_bind_CI_inactive', TNF(bCI=None) + CI(bTNF=None, state='i') | TNF(bCI=1) % CI(bTNF=1, state='i'), kf1, kr1)
 Rule('TNF_CI_dissociate_CI_active', TNF(bCI=1) % CI(bTNF=1, state='i') >> TNF(bCI=None) + CI(bTNF=None, state='a'), kf2)
-Rule('CI_CII_conversion', CI(bTNF=None, state='a') >> CII(bCI=None, state='a'), kf3)
-Rule('CII_MLKL_bind', CII(bCI=None, state='a') + MLKL(bCII=None, state='u') | CII(bCI=1, state='a') % MLKL(bCII=1, state='u'), kf4, kr4)
-Rule('CII_MLKL_dissociate_MLKL_phosphorylated', CII(bCI=1, state='a') % MLKL(bCII=1, state='u') >> MLKL(bCII=None, state='p'), kf5)
+Rule('CI_CII_conversion', CI(bTNF=None, state='a') >> CII(bMLKL=None, state='a'), kf3)
+Rule('CII_MLKL_bind', CII(bMLKL=None, state='a') + MLKL(bCII=None, state='u') | CII(bMLKL=1, state='a') % MLKL(bCII=1, state='u'), kf4, kr4)
+Rule('CII_MLKL_dissociate_MLKL_phosphorylated', CII(bMLKL=1, state='a') % MLKL(bCII=1, state='u') >> MLKL(bCII=None, state='p'), kf5)
 
 # initial conditions
 Parameter('TNF_0', 2326)
@@ -45,11 +45,22 @@ Observable('TNF_obs', TNF(bCI=None))
 Observable('CI_inactive_obs', CI(bTNF=None, state='i'))
 Observable('TNF_CI_CIi_obs', TNF(bCI=1) % CI(bTNF=1, state='i'))
 Observable('CIa_obs', CI(bTNF=None, state='a'))
-Observable('CIIa_obs', CII(bCI=None, state='a'))
-Observable('CII_MLKL_bind_obs', CII(bCI=1, state='a') % MLKL(bCII=1, state='u'))
+Observable('CIIa_obs', CII(bMLKL=None, state='a'))
+Observable('CII_MLKL_bind_obs', CII(bMLKL=1, state='a') % MLKL(bCII=1, state='u'))
 Observable('MLKLu_obs', MLKL(bCII=None, state='u'))
 Observable('MLKLp_obs', MLKL(bCII=None, state='p'))
 
 generate_equations(model)
 
+tspan = np.linspace(0, 1440, 1441) # time span of simulation (start, stop, step)
+result = ScipyOdeSimulator(model, tspan=tspan).run() # run solver of model
+
+# plt.figure()
+# plt.plot(tspan, result.observables['MLKLp_obs'][:], lw = 1.5, label = 'MLKLp_obs') #plot observable
+# plt.xlabel('Time [hours]', fontsize=14)
+# plt.ylabel('Phosphorylated MLKL amount [molecules]', fontsize=14)
+# plt.title('Amount of MLKLp over time')
+# plt.legend(loc = 'best', fontsize = 12) # add legend to plot
+# plt.savefig('pMLKL to paramset14.pdf') # to save figure
+# plt.show()
 
