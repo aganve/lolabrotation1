11b0ca475174622940c6861075e1092772d0b822
aganve <verda.e.agan@vanderbilt.edu>
2019-10-10 14:30:47 -0500

    updated ferroptosis file

--- ferroptosis.py
+++ ferroptosis.py
@@ -19,16 +19,16 @@ Model()
 # GSH = glutathione; cofactor for GPX4 enzyme that reduces lipid peroxides to lipid alcohols
 
 # declare monomers
-Monomer('Cys2', ['b']) # binds cystine reductase (CR) which reduces 1 molecule of cystine to 2 molecules of cysteine
+Monomer('Cys2', ['bCR']) # binds cystine reductase (CR) which reduces 1 molecule of cystine to 2 molecules of cysteine
 # Monomer('XC', ['b']) # no direct binding between Cys2 and XC
 Monomer('CR', ['bCys2'])
 Monomer('Cys', ['bGCL'])
 Monomer('GCL', ['bCys'])
 Monomer('GlutCys', ['bGCL'])
 Monomer('GSS', ['bGlutCys'])
-Monomer('GSH', ['b1Gpx4'], {'state': ['red', 'ox']})
-Monomer('GPX4', ['b1GSH', 'b2Peroxide'], {'state': ['i', 'a']})
-Monomer('Liperox', ['b2GPX4'], {'state': ['ox', 'red']})
+Monomer('GSH', ['b1GPX4','state'], {'state': ['red', 'ox']})
+Monomer('GPX4', ['b1GSH', 'b2Peroxide', 'state'], {'state': ['i', 'a']})
+Monomer('Liperox', ['b2GPX4', 'state'], {'state': ['ox', 'red']})
 
 
 # One main 3d compartment and two 2d membranes inside it
@@ -61,19 +61,19 @@ Parameter('kf7', 1.0e-6)
 # now input the rules
 
 # transport Cys2 (cystine) from the Env into the Cyto
-equilibrate(Cys2(b=None) ** Env, Cys2(b=None) ** Cyto, [kf1, kr1])
+equilibrate(Cys2(bCR=None) ** Env, Cys2(bCR=None) ** Cyto, [kf1, kr1])
 # Rule('Cys2_transport_via_XC', Cys2() ** Env + XC() ** PM >> Cys2() ** Cyto + XC() ** PM, kf1)
 
 # once in the Cyto, Cys2 (cystine) is reduced to Cys via CR catalysis
-catalyze(CR(bCys2=None) ** Cyto, Cys2(bCR=None) ** Cyto, Cys(b=None) ** Cyto, [kf2, kr2, kc2])
+catalyze(CR(bCys2=None) ** Cyto, Cys2(bCR=None) ** Cyto, Cys(bGCL=None) ** Cyto, kf2, kr2, kc2)
 # Rule('Cys2_CR_bind', Cys2(bCR=None) ** Cyto + CR(bCys2=None) ** Cyto | Cys2(bCR=1) ** Cyto % CR(bCys2=1) ** Cyto, kf2, kr2)
 
 # convert Cys to GlutCys via GCL catalysis in the Cyto
-catalyze(GCL(bCys=None) ** Cyto, Cys(bGCL=None) ** Cyto, GlutCys(b=None) ** Cyto,[kf3, kr3, kc3])
+catalyze(GCL(bCys=None) ** Cyto, Cys(bGCL=None) ** Cyto, GlutCys(bGSS=None) ** Cyto, kf3, kr3, kc3)
 # Rule('Cys_GSH_conversion', Cys(bCys2=None) ** Cyto | GSH(bGPX4=None) ** Cyto, kf3, kr2)
 
 # convert Glut-Cys to GSH via GSS catalysis in the Cyto
-catalyze(GSS(bGlutCys=None) ** Cyto, GlutCys(bGSS=None) ** Cyto, GSH(b=None) ** Cyto, [kf4, kr4, kc4])
+catalyze(GSS(bGlutCys=None) ** Cyto, GlutCys(bGSS=None) ** Cyto, GSH(b1GPX4=None, state='ox') ** Cyto, kf4, kr4, kc4)
 
 # GPX4 binds GSH and is activated
 Rule('GSH_binds_GPX4_activated', GSH(b1GPX4=None, state='ox') ** Cyto + GPX4(b1GSH=None, b2Liperox=None, state='i') ** Cyto | GSH(b1GPX4=1, state='ox') ** Cyto % GPX4(b1GSH=1, b2Liperox=None, state='a') ** Cyto, kf5, kr5)
@@ -105,88 +105,91 @@ Initial(Liperox(b2GPX4=None, state='ox') ** Cyto, Liperox_0)
 # Observables
 Observable('Cys2_Env_obs', Cys2(b=None) ** Env)
 Observable('Cys2_Cyto_obs', Cys2(b=None) ** Cyto)
-Observable('CR_Cyto_obs',CR(bCys2=None) ** Cyto)
-Observable('')
-
-Observable('Cys2_XC_bind_obs', Cys2(bXC=1) ** PM % XC(bCys2=1) ** PM)
-Observable('Cys2_Cyto_obs', Cys2(bXC=None) ** Cyto)
-Observable('Cys_obs', Cys(bCys2=None) ** Cyto)
-Observable('GSH_obs', GSH(bGPX4=None) ** Cyto)
-Observable('GPX4_obs', GPX4(bGSH=None) ** Cyto)
-Observable('GSH_GPX4_bind_obs', GSH(bGPX4=1) ** Cyto % GPX4(bGSH=1) ** Cyto)
+Observable('CR_obs', CR(bCys2=None) ** Cyto)
+Observable('Cys_obs', Cys(b=None) ** Cyto)
+Observable('GCL_obs', GCL(bCys=None) ** Cyto)
+Observable('Glut_Cys_obs', GlutCys(b=None) ** Cyto)
+Observable('GSS_obs', GSS(bGlutCys=None) ** Cyto)
+Observable('GSH_unbound_ox_obs', GSH(b1GPX4=None, state='ox') ** Cyto)
+Observable('GSH_unbound_red_obs', GSH(b1GPX4=None, state='red'))
+Observable('GPX4_GSH_unbound_inactive_obs', GPX4(b1GSH=None, b2Liperox=None, state='i') ** Cyto)
+Observable('GPX4_GSH_bound_active_obs', GSH(b1GPX4=1, state='ox') ** Cyto % GPX4(b1GSH=1, b2Liperox=None, state='a') ** Cyto)
+Observable('Liperox_unbound_ox_obs', Liperox(b2GPX4=None, state='ox') ** Cyto)
+Observable('Liperox_unbound_red_obs', Liperox(b2GPX4=None, state='red') ** Cyto)
+Observable('GPX4_GSH_Liperox_complex_obs', GSH(b1GPX4=1, state='red') ** Cyto % GPX4(b1GSH=1, b2Liperox=1, state='a') ** Cyto % Liperox(b2GPX4=1, state='red') ** Cyto)
 
 tspan = np.linspace(0, 1440, 1441) # time span of simulation (start, stop, step)
 result = ScipyOdeSimulator(model, tspan=tspan).run() # run solver of model
 
 plt.figure()
-plt.plot(tspan, result.observables['GSH_GPX4_bind_obs'][:], lw = 1.5, label = 'GSH_GPX4_bind') #plot observable
+plt.plot(tspan, result.observables['Liperox_unbound_red_obs'][:], lw = 1.5, label = 'Liperox (red) molecules') #plot observable
 plt.xlabel('Time [min]', fontsize=14)
-plt.ylabel('Amount of GSH-GPX4 produced [# of molecules]', fontsize=14)
-plt.title('GSH-GPX4 complexes')
+plt.ylabel('Amount of Liperox (red) produced [# of molecules]', fontsize=14)
+plt.title('Liperox (red) molecules')
 plt.legend(loc = 'best', fontsize = 12) # add legend to plot
 # plt.savefig('GSH:GPX4 binding.pdf') # to save figure
 plt.show()
 
-plt.figure()
-plt.plot(tspan, result.observables['GPX4_obs'][:], lw = 1.5, label = 'GPX4') #plot observable
-plt.xlabel('Time [min]', fontsize=14)
-plt.ylabel('Amount of GPX4 utilized [# of molecules]', fontsize=14)
-plt.title('Amount of GPX4 utilized over time')
-plt.legend(loc = 'best', fontsize = 12) # add legend to plot
-# plt.savefig('GPX4.pdf') # to save figure
-plt.show()
+# plt.figure()
+# plt.plot(tspan, result.observables['GPX4_obs'][:], lw = 1.5, label = 'GPX4') #plot observable
+# plt.xlabel('Time [min]', fontsize=14)
+# plt.ylabel('Amount of GPX4 utilized [# of molecules]', fontsize=14)
+# plt.title('Amount of GPX4 utilized over time')
+# plt.legend(loc = 'best', fontsize = 12) # add legend to plot
+# # plt.savefig('GPX4.pdf') # to save figure
+# plt.show()
 
-plt.figure()
-plt.plot(tspan, result.observables['GSH_obs'][:], lw = 1.5, label = 'GSH') #plot observable
-plt.xlabel('Time [min]', fontsize=14)
-plt.ylabel('Amount of GSH utilized [# of molecules]', fontsize=14)
-plt.title('Amount of GSH utilized over time')
-plt.legend(loc = 'best', fontsize = 12) # add legend to plot
-# plt.savefig('GSH.pdf') # to save figure
-plt.show()
+# plt.figure()
+# plt.plot(tspan, result.observables['GSH_obs'][:], lw = 1.5, label = 'GSH') #plot observable
+# plt.xlabel('Time [min]', fontsize=14)
+# plt.ylabel('Amount of GSH utilized [# of molecules]', fontsize=14)
+# plt.title('Amount of GSH utilized over time')
+# plt.legend(loc = 'best', fontsize = 12) # add legend to plot
+# # plt.savefig('GSH.pdf') # to save figure
+# plt.show()
 
-plt.figure()
-plt.plot(tspan, result.observables['Cys_obs'][:], lw = 1.5, label = 'Cys') #plot observable
-plt.xlabel('Time [min]', fontsize=14)
-plt.ylabel('Amount of Cys produced [# of molecules]', fontsize=14)
-plt.title('Amount of Cys produced over time')
-plt.legend(loc = 'best', fontsize = 12) # add legend to plot
-# plt.savefig('Cys.pdf') # to save figure
-plt.show()
+# plt.figure()
+# plt.plot(tspan, result.observables['Cys_obs'][:], lw = 1.5, label = 'Cys') #plot observable
+# plt.xlabel('Time [min]', fontsize=14)
+# plt.ylabel('Amount of Cys produced [# of molecules]', fontsize=14)
+# plt.title('Amount of Cys produced over time')
+# plt.legend(loc = 'best', fontsize = 12) # add legend to plot
+# # plt.savefig('Cys.pdf') # to save figure
+# plt.show()
 
-plt.figure()
-plt.plot(tspan, result.observables['Cys2_Cyto_obs'][:], lw = 1.5, label = 'Cys2') #plot observable
-plt.xlabel('Time [min]', fontsize=14)
-plt.ylabel('Amount of Cys2 converted [# of molecules]', fontsize=14)
-plt.title('Amount of Cys2 consumed over time')
-plt.legend(loc = 'best', fontsize = 12) # add legend to plot
-# plt.savefig('Cys2.pdf') # to save figure
-plt.show()
+# plt.figure()
+# plt.plot(tspan, result.observables['Cys2_Cyto_obs'][:], lw = 1.5, label = 'Cys2') #plot observable
+# plt.xlabel('Time [min]', fontsize=14)
+# plt.ylabel('Amount of Cys2 converted [# of molecules]', fontsize=14)
+# plt.title('Amount of Cys2 consumed over time')
+# plt.legend(loc = 'best', fontsize = 12) # add legend to plot
+# # plt.savefig('Cys2.pdf') # to save figure
+# plt.show()
 
-plt.figure()
-plt.plot(tspan, result.observables['Cys2_XC_bind_obs'][:], lw = 1.5, label = 'Cys2_XC_bind') #plot observable
-plt.xlabel('Time [min]', fontsize=14)
-plt.ylabel('Amount of Cys2-XC complexes [# of molecules]', fontsize=14)
-plt.title('Amount of Cys2-XC complexes over time')
-plt.legend(loc = 'best', fontsize = 12) # add legend to plot
-# plt.savefig('Cys2_XC_bind.pdf') # to save figure
-plt.show()
+# plt.figure()
+# plt.plot(tspan, result.observables['Cys2_XC_bind_obs'][:], lw = 1.5, label = 'Cys2_XC_bind') #plot observable
+# plt.xlabel('Time [min]', fontsize=14)
+# plt.ylabel('Amount of Cys2-XC complexes [# of molecules]', fontsize=14)
+# plt.title('Amount of Cys2-XC complexes over time')
+# plt.legend(loc = 'best', fontsize = 12) # add legend to plot
+# # plt.savefig('Cys2_XC_bind.pdf') # to save figure
+# plt.show()
 
-plt.figure()
-plt.plot(tspan, result.observables['XC_obs'][:], lw = 1.5, label = 'XC') #plot observable
-plt.xlabel('Time [min]', fontsize=14)
-plt.ylabel('Amount of XC proteins being bound [# of molecules]', fontsize=14)
-plt.title('Amount of XC proteins being bound over time')
-plt.legend(loc = 'best', fontsize = 12) # add legend to plot
-# plt.savefig('XC.pdf') # to save figure
-plt.show()
+# plt.figure()
+# plt.plot(tspan, result.observables['XC_obs'][:], lw = 1.5, label = 'XC') #plot observable
+# plt.xlabel('Time [min]', fontsize=14)
+# plt.ylabel('Amount of XC proteins being bound [# of molecules]', fontsize=14)
+# plt.title('Amount of XC proteins being bound over time')
+# plt.legend(loc = 'best', fontsize = 12) # add legend to plot
+# # plt.savefig('XC.pdf') # to save figure
+# plt.show()
 
-plt.figure()
-plt.plot(tspan, result.observables['Cys2_Env_obs'][:], lw = 1.5, label = 'Cys2_Env') #plot observable
-plt.xlabel('Time [min]', fontsize=14)
-plt.ylabel('Amount of Cys2 proteins binding to XC [# of molecules]', fontsize=14)
-plt.title('Amount of Cys2 protein binding to XC over time')
-plt.legend(loc = 'best', fontsize = 12) # add legend to plot
-# plt.savefig('Cys2_Env.pdf') # to save figure
-plt.show()
+# plt.figure()
+# plt.plot(tspan, result.observables['Cys2_Env_obs'][:], lw = 1.5, label = 'Cys2_Env') #plot observable
+# plt.xlabel('Time [min]', fontsize=14)
+# plt.ylabel('Amount of Cys2 proteins binding to XC [# of molecules]', fontsize=14)
+# plt.title('Amount of Cys2 protein binding to XC over time')
+# plt.legend(loc = 'best', fontsize = 12) # add legend to plot
+# # plt.savefig('Cys2_Env.pdf') # to save figure
+# plt.show()
 
